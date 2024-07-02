9339a23349997a5266d6a7985c66c4ee9237c19c
irapanch <117355580+irapanch@users.noreply.github.com>
2024-07-02 15:07:54 +0300

    clear

--- constants/constants.js
+++
@@ -1,7 +0,0 @@
-const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-const allowedSubscriptions = ["starter", "pro", "business"];
-
-module.exports = {
-  emailRegexp,
-  allowedSubscriptions,
-};
